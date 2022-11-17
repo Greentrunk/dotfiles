75fa335ecc20d498bb06ff95b701d99b835484bf
CJPohl <cjpohldev@gmail.com>
2022-11-17 09:53:09 -0500

    changed fonts

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,17 +1,14 @@
-local impatient_ok, impatient = pcall(require, "impatient")
-if impatient_ok then impatient.enable_profile() end
+vim.opt.number = true
+vim.opt.mouse = 'a'
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.hlsearch = false
+vim.opt.wrap = true
+vim.opt.breakindent = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = false
 
-for _, source in ipairs {
-  "core.utils",
-  "core.options",
-  "core.bootstrap",
-  "core.plugins",
-  "core.autocmds",
-  "core.mappings",
-  "configs.which-key-register",
-} do
-  local status_ok, fault = pcall(require, source)
-  if not status_ok then vim.api.nvim_err_writeln("Failed to load " .. source .. "\n\n" .. fault) end
-end
+vim.g.mapleader = ' '
+vim.keymap.set('n', '<leader>w', '<cmd>write<cr>', {desc = 'Save'})
 
-astronvim.conditional_func(astronvim.user_plugin_opts("polish", nil, false))
