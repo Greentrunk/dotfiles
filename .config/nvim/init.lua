9f59004046f9e965360814fec7a55a33bcbe727d
CJPohl <cjpohldev@gmail.com>
2022-11-21 12:15:29 -0500

    started custom neovim config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,14 +1,53 @@
-vim.opt.number = true
-vim.opt.mouse = 'a'
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-vim.opt.hlsearch = false
-vim.opt.wrap = true
-vim.opt.breakindent = true
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
-vim.opt.expandtab = false
+-- core
+require("user.plugins")
+require("user.options")
+require("user.keymaps")
 
-vim.g.mapleader = ' '
-vim.keymap.set('n', '<leader>w', '<cmd>write<cr>', {desc = 'Save'})
+-- persist colorscheme
+require("user.colorscheme")
 
+-- cmp
+require("user.cmp")
+
+-- lsp
+require("user.lsp")
+
+-- Telescope
+require("user.telescope")
+
+-- Cheatcheet
+require("user.cheatsheet")
+
+-- Treesitter
+require("user.treesitter")
+require("user.project")
+
+-- autopairs
+require("user.autopairs")
+
+-- commenting
+require("user.comment")
+
+-- git
+require("user.gitsigns")
+
+-- Nvim tree
+require("user.nvim-tree")
+
+-- bufferline
+require("user.bufferline")
+
+-- lualine
+require("user.lualine")
+
+-- toggleterm
+require("user.toggleterm")
+
+-- impatient
+require("user.impatient")
+
+-- indent_blankline
+require("user.indent-blankline")
+
+-- alpha
+require("user.alpha")
---
+++ .config/nvim/lua/user/colorscheme.lua
@@ -0,0 +1,8 @@
+-- vim.cmd "colorscheme gruvbox"
+local colorscheme = "gruvbox"
+
+local status_ok = pcall(vim.cmd, "colorscheme " .. colorscheme)
+if not status_ok then
+  vim.notify("colorscheme " .. colorscheme.. " not found!")
+  return
+end
